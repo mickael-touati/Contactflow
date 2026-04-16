55f5415978af87b54289d64b54e10d93f2a821c5
Mickael Touati <mickael.touati@laplateforme.io>
2026-04-16 15:24:18 +0200

    push page

--- front/page/detail.js
+++ front/page/detail.js
@@ -3,5 +3,6 @@ export default function() {
         <h1>Détail du contact</h1>
         <a href="/Contactflow/front/">Retour</a>
         <div id="detail-contact">chargement...</div>
+        <a href="/Contactflow/front/edit">Modifier ce contact</a>
     `
 }
