e68d1338d2c32a6671309da635d3c0277d687a94
Gabriel Sempéré <gabriel.sempere@laplateforme.io>
2026-04-16 14:24:15 +0200

    create ContactManager and updateProfile

--- src/Managers/ContactManager.php
+++ src/Managers/ContactManager.php
@@ -5,8 +5,51 @@ use PDO;
 
 class ContactManager extends Database {
 
-public function addContact ($name, $password, )
+
+    public function addContact($nom, $prenom, $email, $phone, $favoris, $userId) {
+        $sql = "INSERT INTO contacts (nom, prenom, email, phone, favoris, user_id) VALUES (:nom, :prenom, :email, :phone, :favoris, :user_id)";
+        $stmt = $this->pdo->prepare($sql);
+        return $stmt->execute([
+            'nom' => $nom,
+            'prenom' => $prenom,
+            'email' => $email,
+            'phone' => $phone,
+            'favoris' => $favoris,
+            'user_id' => $userId
+        ]);
+    }
+
+    public function getContactsByUser($userId) {
+        $sql = "SELECT * FROM contacts WHERE user_id = :user_id";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute(['user_id' => $userId]);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
 
 
+    public function getContactById($id) {
+        $sql = "SELECT * FROM contacts WHERE id = :id";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute(['id' => $id]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function updateContact($id, $nom, $prenom, $email, $phone, $favoris) {
+        $sql = "UPDATE contacts SET nom = :nom, prenom = :prenom, email = :email, phone = :phone, favoris = :favoris WHERE id = :id";
+        $stmt = $this->pdo->prepare($sql);
+        return $stmt->execute([
+            'nom' => $nom,
+            'prenom' => $prenom,
+            'email' => $email,
+            'phone' => $phone,
+            'favoris' => $favoris
+        ]);
+    }
+
+    public function deleteContact($id) {
+        $sql = "DELETE FROM contacts WHERE id = :id";
+        $stmt = $this->pdo->prepare($sql);
+        return $stmt->execute(['id' => $id]);
+    }
 }
-php?>
+?>
--- src/Managers/UserManager.php
+++ src/Managers/UserManager.php
@@ -33,4 +33,26 @@ class UserManager extends Database {
         $stmt->execute(['id' => $id]);
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
+
+    public function updateProfile($id, $username, $password = null) {
+        if (!empty($password)) { 
+            $hash = password_hash($password, PASSWORD_BCRYPT);
+            $sql = "UPDATE user SET username = :username, password = :password WHERE id = :id";
+            $stmt = $this->pdo->prepare($sql);
+            return $stmt->execute([
+                'id' => $id,
+                'username' => $username,
+                'password' => $hash
+            ]);
+        } 
+        else {
+            $sql = "UPDATE user SET username = :username WHERE id = :id";
+            $stmt = $this->pdo->prepare($sql);
+            return $stmt->execute([
+                'id' => $id,
+                'username' => $username
+            ]);
+        }
+    }
 }
+?>
